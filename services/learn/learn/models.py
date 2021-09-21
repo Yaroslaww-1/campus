f053cf1a63b5ad6a9459c8d77952eef4e1d65c8c
fireman9 <nazariystr@gmail.com>
2021-09-21 20:44:06 +0300

    CS-13: created table user in models

--- services/learn/learn/models.py
+++ services/learn/learn/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Post(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=100)
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
