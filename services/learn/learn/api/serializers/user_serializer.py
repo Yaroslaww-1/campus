578edf646f75d6d4dfc16c7c952df077dfc5c097
fireman9 <nazariystr@gmail.com>
2021-09-22 20:03:11 +0300

    CS-13: added email validation

--- services/learn/learn/api/serializers/user_serializer.py
+++ services/learn/learn/api/serializers/user_serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.Serializer):
     id = serializers.CharField()
     name = serializers.CharField()
-    email = serializers.CharField()
+    email = serializers.EmailField()
