f89864130ed11881d7db6bb291fc618bd44c5396
fireman9 <nazariystr@gmail.com>
2021-09-28 20:30:18 +0300

    CS-18: added route to view users list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Switch } from "react-router-dom";
 
 import { AppRoute } from "@common/enums/app-route.enum";
 
-import { HomePage, PostsPage } from "./pages";
+import { HomePage, PostsPage, UsersPage } from "./pages";
 
 export const App = () => {
   return (
     <Switch>
       <Route exact path={AppRoute.HOME} component={HomePage} />
       <Route path={AppRoute.POSTS} component={PostsPage} />
+      <Route path={AppRoute.USERS} component={UsersPage} />
     </Switch>
   );
 };
--- frontend/src/common/enums/app-route.enum.ts
+++ frontend/src/common/enums/app-route.enum.ts
@@ -1,4 +1,5 @@
 export enum AppRoute {
   HOME = "/",
   POSTS = "/posts",
+  USERS = "/users"
 }
