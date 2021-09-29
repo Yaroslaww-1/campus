832ebff658e9440250ac4e43b7b38d1181cb6d29
fireman9 <nazariystr@gmail.com>
2021-09-29 13:03:10 +0300

    CS-18: index updated

--- frontend/src/pages/chat/index.tsx
+++ frontend/src/pages/chat/index.tsx
@@ -1,16 +1,10 @@
 import { PageComponent } from "@components/index";
-import { UserList } from "./components/users-list.component";
+import { UsersListContainer } from "./containers/user-list";
 
 export const UsersPage: React.FC = () => {
-  const userList = [
-    { id: "1", name: "Nazarii Striletskyi", role: "student", status: "online" },
-    { id: "2", name: "Yaroslav Borodaienko", role: "student", status: "online" },
-    { id: "3", name: "Clown", role: "lecturer", status: "offline" },
-    { id: "4", name: "Circus", role: "admin", status: "offline" },
-  ];
   return (
     <PageComponent>
-      <UserList userList={userList} />
+      <UsersListContainer />
     </PageComponent>
   );
 };
