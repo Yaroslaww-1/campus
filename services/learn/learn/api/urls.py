c88fb2e475e0a7cf849509b25d40fa9f145a180a
fireman9 <nazariystr@gmail.com>
2021-10-01 15:41:23 +0300

    CS-14: added group url patterns to urls

--- services/learn/learn/api/urls.py
+++ services/learn/learn/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
+from .views.groups_view import groups_urlpatterns
 from .views.posts_view import posts_urlpatterns
 from .views.users_view import users_urlpatterns
 
-urlpatterns = [] + posts_urlpatterns + users_urlpatterns
+urlpatterns = [] + posts_urlpatterns + users_urlpatterns + groups_urlpatterns
