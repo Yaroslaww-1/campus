c75da0311ecfd5fd20ed7aa1e11bc74073db42ad
VladyslavSkrypets <vladskripets@gmail.com>
2021-10-10 20:16:45 +0300

    add runner for gathering and fix gathering error

--- services/schedule/parser/gather.py
+++ services/schedule/parser/gather.py
@@ -1,22 +1,24 @@
 import re
+import os
 import json
 import aiohttp
 import asyncio
 from config import *
+from os import path
 from bs4 import BeautifulSoup
 
 
-# async def fetch_groups(session) -> dict:
-#     """Here we get list of all groups that starts from 'ІП-'.
-#     """
-#     payload = {
-#         'prefixText': 'ІП-',
-#         'count': 10
-#     }
-#     async with session.post(GROUPS_URL, json=payload) as response:
-#         if not response.ok:
-#             response.raise_for_status()
-#         return await response.json()
+async def fetch_groups(session) -> dict:
+    """Here we get list of all groups that starts from 'ІП-'.
+    """
+    payload = {
+        'prefixText': 'ІП-',
+        'count': 10
+    }
+    async with session.post(GROUPS_URL, json=payload) as response:
+        if not response.ok:
+            response.raise_for_status()
+        return await response.json()
 
 
 async def fetch_group_schedule(session, group) -> str:
@@ -54,12 +56,12 @@ async def get_pairs_info(html_list: list[BeautifulSoup]) -> list:
                 for idx in range(len(pairs_block)):
                     list_of_pairs.append({
                         "is_pair": True,
-                        "day": days[index],
+                        "day": days[index] if days else [],
                         "pair_number": pair_number,
-                        "pair_name": pairs_block[idx].get('title'),
-                        "pair_link": pairs_block[idx].get('href').replace(' ', '_'),
-                        "learner_name": learners_block[idx].get('title'),
-                        "learner_link": f"http://rozklad.kpi.ua{learners_block[idx].get('href')}"
+                        "pair_name": pairs_block[idx].get('title') if pairs_block else '',
+                        "pair_link": pairs_block[idx].get('href').replace(' ', '_') if pairs_block else '',
+                        "learner_name": learners_block[idx].get('title') if learners_block else '',
+                        "learner_link": f"http://rozklad.kpi.ua{learners_block[idx].get('href')}" if learners_block else ''
                     })
             except AttributeError:
                 list_of_pairs.append({
@@ -76,13 +78,19 @@ async def get_pairs_info(html_list: list[BeautifulSoup]) -> list:
 
 async def get_site_content(html) -> dict:
     soup = BeautifulSoup(html.decode('utf-8'), 'html.parser')
-    group = re.sub(r'Розклад занять для\s*', '', soup.find('span', attrs={'id': 'ctl00_MainContent_lblHeader'}).text)
+    group_text = soup.find('span', attrs={'id': 'ctl00_MainContent_lblHeader'})
+    group = re.sub(r'Розклад занять для\s*', '', group_text.text if group_text else '')
+    print(group)
 
-    first_week = soup.find('table', attrs={'id': 'ctl00_MainContent_FirstScheduleTable'}).find_all('tr')
-    pairs_info_fw = await get_pairs_info(first_week)
+    try:
+        first_week = soup.find('table', attrs={'id': 'ctl00_MainContent_FirstScheduleTable'}).find_all('tr')
+        pairs_info_fw = await get_pairs_info(first_week)
 
-    second_week = soup.find('table', attrs={'id': 'ctl00_MainContent_SecondScheduleTable'}).find_all('tr')
-    pairs_info_sw = await get_pairs_info(second_week)
+        second_week = soup.find('table', attrs={'id': 'ctl00_MainContent_SecondScheduleTable'}).find_all('tr')
+        pairs_info_sw = await get_pairs_info(second_week)
+    except Exception:
+        pairs_info_fw, pairs_info_sw = [], []
+
     return {
         "group": group,
         "first_week": pairs_info_fw,
@@ -90,20 +98,21 @@ async def get_site_content(html) -> dict:
     }
 
 
-async def main():
+async def main(path_dir: str):
     timeout = aiohttp.ClientTimeout(total=60)
     async with aiohttp.ClientSession(timeout=timeout) as session:
-        # groups = await fetch_groups(session)
-        # htmls = await fetch_all_group_schedule(session, groups.get('d', []))
-        groups = ['ІП-96']
-        htmls = await fetch_all_group_schedule(session, groups)
+        groups = await fetch_groups(session)
+        htmls = await fetch_all_group_schedule(session, groups.get('d', []))
         for idx, html in enumerate(htmls):
             content = await get_site_content(html)
-            with open(f"./schedule/{content.get('group', idx)}.json", "w", encoding='utf-8') as json_file:
+            with open(f"{path_dir}/{content.get('group', idx).strip()}.json", "w") as json_file:
                 json.dump(content, json_file, indent=2)
 
 
-if __name__ == '__main__':
+def gather_run():
+    data_directory = f"{os.getcwd()}/scheduleData/"
+    if not path.exists(data_directory):
+        os.mkdir(data_directory)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(data_directory))
     loop.close()
