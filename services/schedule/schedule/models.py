979559bc0b100cc7e4d90158aa5c35d590568b9e
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-10-11 22:42:53 +0300

    CS-22: updated schedule schema

--- services/schedule/schedule/models.py
+++ services/schedule/schedule/models.py
@@ -5,6 +5,39 @@ from django.db import models
 # https://docs.djangoproject.com/en/3.2/ref/models/fields/
 
 
+class Role(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "role"
+
+    objects = models.Manager()
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    name = models.CharField(max_length=100)
+
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "user"
+
+    objects = models.Manager()
+
+
+class Student(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "student"
+
+    objects = models.Manager()
+
+
 class EventType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4())
     name = models.CharField(max_length=100)
@@ -15,36 +48,28 @@ class EventType(models.Model):
     objects = models.Manager()
 
 
-class Event(models.Model):
+class EventTemplate(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4())
     name = models.CharField(max_length=100)
     description = models.TextField()
-    time = models.DateTimeField()
+
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.ForeignKey(EventType, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "event"
+        db_table = "event_template"
 
     objects = models.Manager()
 
 
-class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
-    name = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = "user"
-
-    objects = models.Manager()
-
-
-class Teacher(models.Model):
+class Event(models.Model):
     id = models.UUIDField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    events = models.ManyToManyField(Event, db_table="teacher_to_event")
+    time = models.DateTimeField()
+
+    template = models.ForeignKey(EventTemplate, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "teacher"
+        db_table = "event"
 
     objects = models.Manager()
 
