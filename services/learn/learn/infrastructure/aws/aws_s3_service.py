db3e75a115bbe25e13950c9828d19a932fd61eea
fireman9 <nazariystr@gmail.com>
2021-10-23 21:07:20 +0300

    CS-29: fixed string problem

--- services/learn/learn/infrastructure/aws/aws_s3_service.py
+++ services/learn/learn/infrastructure/aws/aws_s3_service.py
@@ -9,13 +9,8 @@ class AwsS3Service:
         AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
         AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
         if prevUrl is not None:
-            path = prevUrl.replace(
-                "https://s3-{0}.amazonaws.com/{1}/".format(AWS_REGION_NAME, AWS_BUCKET_NAME), "")
+            path = prevUrl.replace(f"https://s3-{AWS_REGION_NAME}.amazonaws.com/{AWS_BUCKET_NAME}/", "")
             default_storage.delete(path)
         filepath = default_storage.save("media/" + id + "/" + file.name, file)
-        fileUrl = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
-            AWS_REGION_NAME,
-            AWS_BUCKET_NAME,
-            filepath
-        )
+        fileUrl = f"https://s3-{AWS_REGION_NAME}.amazonaws.com/{AWS_BUCKET_NAME}/{filepath}"
         return fileUrl
