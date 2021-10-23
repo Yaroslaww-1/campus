33816ec0f1f867629f38be1f58ee9c30d7bb9423
fireman9 <nazariystr@gmail.com>
2021-10-23 20:08:49 +0300

    CS-29: remade form with using mobx

--- frontend/src/pages/user/components/update-user-avatar.component.tsx
+++ frontend/src/pages/user/components/update-user-avatar.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import api from "../../../api/api.helper";
 
@@ -8,22 +8,33 @@ export const UpdateUserAvatar: React.FC<User> = props => {
   const BASE_URL = process.env.REACT_APP_API_URL;
   const endpoint = "users";
 
+  const [id, setId] = useState("");
+  const [avatar, setAvatar] = useState<File>();
+
+  function onSubmit() {
+    const formData = new FormData();
+    formData.append("id", id);
+    formData.append("avatar", avatar || "");
+    api.put(BASE_URL + endpoint, formData);
+  }
+
   return (
     <div>
-      <form
-        id="formEl"
-        onSubmit={() => {
-          api.put(
-            BASE_URL + endpoint,
-            new FormData(document.querySelector("form") || undefined),
-          );
-        }}
-      >
+      <form id="formEl" onSubmit={onSubmit}>
         <label>Id: </label>
-        <input type="text" name="id" />
+        <input
+          type="text"
+          name="id"
+          value={id}
+          onChange={e => setId(e.target.value)}
+        />
         <br />
         <label>Filename: </label>
-        <input type="file" name="avatar" />
+        <input
+          type="file"
+          name="avatar"
+          onChange={e => setAvatar(e.target.files![0])}
+        />
         <br />
         <input type="submit" name="submit" value="Submit" />
       </form>
